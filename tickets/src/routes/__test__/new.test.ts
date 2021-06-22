2d7aff81afbdc55762d57c56adf15e68f5a35e31
Sergey Filenko <filenko45@gmail.com>
2021-06-22 13:40:47 +0300

    tickets service

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -3,36 +3,73 @@ import { app } from '../../app';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app)
-      .post('/api/tickets')
-      .send({});
+    .post('/api/tickets')
+    .send({});
   
   expect(response.status).not.toEqual(404);
 });
 
 it('can only be accessed if the user is signed in', async () => {
   await request(app)
-      .post('/api/tickets')
-      .send({})
-      .expect(401);
+    .post('/api/tickets')
+    .send({})
+    .expect(401);
 });
 
 it('returns a status other than 401 if the user is signed in', async () => {
   const response = await request(app)
-      .post('/api/tickets')
-      .set('Cookie', global.signin())
-      .send({});
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({});
   
   expect(response.status).not.toEqual(401);
 });
 
 it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10
+    })
+    .expect(400);
   
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10
+    })
+    .expect(400);
 });
 
 it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'fsafds',
+      price: -10
+    })
+    .expect(400);
   
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'dfdsfs',
+    })
+    .expect(400);
 });
 
 it('creates a ticket with valid inputs', async () => {
-  
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'fsafds',
+      price: 10
+    })
+    .expect(201);
 });
