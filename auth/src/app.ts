71fba29d829f97d9e598c173ab440a84ba32c092
Sergey Filenko <filenko45@gmail.com>
2021-09-30 01:38:13 +0300

    Change

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
