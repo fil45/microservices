93e32a118cca71a41039aa8095c527818b9b1cc9
Sergey Filenko <filenko45@gmail.com>
2021-09-30 01:39:11 +0300

    Change

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -26,7 +25,7 @@ app.use(signupRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
-})
+});
 
 app.use(errorHandler);
 
